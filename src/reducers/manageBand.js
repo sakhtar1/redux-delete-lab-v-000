3b40b559429d52454940d21bb6548d775d4d7948
Sharmina Akhtar <sakht318@gmail.com>
2018-10-25 12:10:34 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,12 +3,14 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-    const addband = {
+    const addBand = {
       id: Math.random()*10000000000000000,
       name: action.name
     }
 
-      return { ...state, bands: [...state.bands, action.name] }
+      return { ...state, bands: [...state.bands, addBand] }
+    case 'DELETE':
+     return { bands: ...state.bands.filter(addBand => addBand.id !== action.band) }:
 
     default:
       return state;
