7e90dcf6b271cd84fd74b0d2c2bdcc2d840b7c25
Sharmina Akhtar <sakht318@gmail.com>
2018-10-25 12:08:04 +0000

    automatically backed up by learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,6 +3,10 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+    const addband = {
+      id: Math.random()*10000000000000000,
+      name: action.name
+    }
 
       return { ...state, bands: [...state.bands, action.name] }
 
